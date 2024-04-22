32ef31b09bca31a9e328aca15dd447ddb5a4e446
Henry Darana <h.darana@gmail.com>
2024-04-22 09:33:28 +0530

    GamePlayAnalysisI, SearchInsertPosition and ValidPhoneNumbers

---
+++ src/main/java/algorithms/SearchInsertPosition.java
@@ -0,0 +1,31 @@
+package algorithms;
+
+public class SearchInsertPosition {
+
+    public static void main(String[] args) {
+        SearchInsertPosition searchInsertPosition = new SearchInsertPosition();
+
+        int[] intArray = new int[]{1, 3, 5, 6};
+        System.out.println("Output:\t" + searchInsertPosition.searchInsert(intArray, 5));
+
+        intArray = new int[]{1, 3, 5, 6};
+        System.out.println("Output:\t" + searchInsertPosition.searchInsert(intArray, 7));
+    }
+
+    public int searchInsert(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int middle = (start + end) / 2;
+            if (nums[middle] == target) {
+                return middle;
+            } else if (nums[middle] > target) {
+                end = middle - 1;
+            } else {
+                start = middle + 1;
+            }
+        }
+        return start;
+    }
+}
---
+++ src/main/java/shell/ValidPhoneNumbers.sh
@@ -0,0 +1,2 @@
+#https://leetcode.com/problems/valid-phone-numbers/description/
+grep -P '^(\d{3}-|\(\d{3}\) )\d{3}-\d{4}$' file.txt
