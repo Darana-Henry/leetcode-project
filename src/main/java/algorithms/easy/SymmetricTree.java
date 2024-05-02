474bc52ff3f05006c37b56368b45b8e9d18d8b13
Henry Darana <h.darana@gmail.com>
2024-05-02 11:22:58 +0530

    AllowOneFunctionCall, SwapSalary and SymmetricTree

---
+++ src/main/java/algorithms/easy/SymmetricTree.java
@@ -0,0 +1,32 @@
+package algorithms.easy;//https://leetcode.com/problems/symmetric-tree/
+
+import context.TreeNode;
+
+public class SymmetricTree {
+
+    public static void main(String[] args) {
+
+        SymmetricTree symmetricTree = new SymmetricTree();
+
+        TreeNode exampleOne = new TreeNode(1, new TreeNode(2, new TreeNode(3), new TreeNode(4)),
+                new TreeNode(2, new TreeNode(4), new TreeNode(3)));
+        System.out.println("Output:\t" + symmetricTree.isSymmetric(exampleOne));
+
+        TreeNode exampleTwo = new TreeNode(1, new TreeNode(2, null, new TreeNode(3)),
+                new TreeNode(2, null, new TreeNode(3)));
+        System.out.println("Output:\t" + symmetricTree.isSymmetric(exampleTwo));
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        return isSame(root, root);
+    }
+
+    public boolean isSame(TreeNode left, TreeNode right) {
+        if (left == null && right == null) return true;
+        if (left == null || right == null) return false;
+
+        return (left.val == right.val)
+                && isSame(left.left, right.right)
+                && isSame(left.right, right.left);
+    }
+}
