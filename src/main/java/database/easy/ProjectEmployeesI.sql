7fc885727ef6089cdcae38b1e68e1a46c4709352
Henry Darana <h.darana@gmail.com>
2024-05-05 09:41:32 +0530

    ArrayWrapper, BalancedBinaryTree and ProjectEmployeesI

---
+++ src/main/java/database/easy/ProjectEmployeesI.sql
@@ -0,0 +1,7 @@
+--https://leetcode.com/problems/project-employees-i/submissions/1249637029/
+
+select p.project_id, round(avg(experience_years),2) as average_years
+from Project p
+join Employee e
+on p.employee_id = e.employee_id
+group by p.project_id
