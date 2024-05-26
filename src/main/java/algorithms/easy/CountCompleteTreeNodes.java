e60bce559d002410eb02902a80232649834dedff
Henry Darana <h.darana@gmail.com>
2024-05-26 09:40:24 +0530

    CountCompleteTreeNodes, ExecuteAsynchronousFunctionsInParallel and FindFollowersCount

---
+++ src/main/java/algorithms/easy/CountCompleteTreeNodes.java
@@ -0,0 +1,27 @@
+//https://leetcode.com/problems/count-complete-tree-nodes/description/
+package algorithms.easy;
+
+import context.TreeNode;
+
+public class CountCompleteTreeNodes {
+
+    public static void main(String[] args) {
+
+        CountCompleteTreeNodes countNodes = new CountCompleteTreeNodes();
+
+        TreeNode root = new TreeNode(1, new TreeNode(2, new TreeNode(4), new TreeNode(5)),
+                new TreeNode(3, new TreeNode(6), null));
+        System.out.println("Output:\t" + countNodes.countNodes(root));
+
+        TreeNode root1 = new TreeNode(1);
+        System.out.println("Output:\t" + countNodes.countNodes(root1));
+    }
+
+    public int countNodes(TreeNode root) {
+        if (root == null)
+            return 0;
+
+        int count = 1;
+        return count + countNodes(root.left) + countNodes(root.right);
+    }
+}
