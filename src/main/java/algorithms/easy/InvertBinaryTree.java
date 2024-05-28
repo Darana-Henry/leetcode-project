8ba1f3dfdaab2c37790123eac040a593a3c36b04
Henry Darana <h.darana@gmail.com>
2024-05-28 10:18:24 +0530

    FindTotalTimeSpentByEachEmployee, InvertBinaryTree and MemoizeII

---
+++ src/main/java/algorithms/easy/InvertBinaryTree.java
@@ -0,0 +1,30 @@
+//https://leetcode.com/problems/invert-binary-tree/description/
+package algorithms.easy;
+
+import context.TreeNode;
+
+public class InvertBinaryTree {
+
+    public static void main(String[] args) {
+
+        InvertBinaryTree invertTree = new InvertBinaryTree();
+
+        TreeNode root = new TreeNode(4, new TreeNode(2, new TreeNode(1), new TreeNode(3)),
+                new TreeNode(7, new TreeNode(6), new TreeNode(9)));
+        System.out.println("Output:\t" + invertTree.invertTree(root));
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null)
+            return null;
+
+        TreeNode ephemeralNode = root.left;
+        root.left = root.right;
+        root.right = ephemeralNode;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+}
