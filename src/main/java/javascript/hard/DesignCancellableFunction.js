0e49041d5390bf220d5c307e6d101522c3ae4257
Henry Darana <h.darana@gmail.com>
2024-05-29 08:27:17 +0530

    DesignCancellableFunction, MergeStringsAlternately and RecyclableAndLowFatProducts

---
+++ src/main/java/javascript/hard/DesignCancellableFunction.js
@@ -0,0 +1,25 @@
+//https://leetcode.com/problems/design-cancellable-function/description/
+
+/**
+ * @param {Generator} generator
+ * @return {[Function, Promise]}
+ */
+var cancellable = function (generator) {
+    var cancel;
+    const cancelPromise = new Promise((_, reject) => { cancel = () => reject("Cancelled"); });
+    cancelPromise.catch(() => { });
+
+    const promise = (async () => {
+        let next = generator.next();
+        while (!next.done) {
+            try {
+                next = generator.next(await Promise.race([next.value, cancelPromise]));
+            } catch (e) {
+                next = generator.throw(e);
+            }
+        }
+        return next.value;
+    })();
+
+    return [cancel, promise];
+};
