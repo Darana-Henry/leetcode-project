3745b6e59691229dd71ed4b455e50a2e272b6721
Henry Darana <h.darana@gmail.com>
2024-06-29 20:23:33 +0530

    AllAncestorsOfANodeInADirectedAcyclicGraph, CountGoodNodesInBinaryTree, DepartmentTopThreeSalaries, MaximumTotalImportanceOfRoads and MaximumTwinSumOfALinkedList

---
+++ src/main/java/algorithms/medium/MaximumTotalImportanceOfRoads.java
@@ -0,0 +1,30 @@
+//https://leetcode.com/problems/maximum-total-importance-of-roads/description/
+package algorithms.medium;
+
+import java.util.Arrays;
+
+public class MaximumTotalImportanceOfRoads {
+    public static void main(String[] args) {
+        MaximumTotalImportanceOfRoads roads = new MaximumTotalImportanceOfRoads();
+        System.out.println(
+                "Output:\t" + roads.maximumImportance(5, new int[][]{{0, 1}, {1, 2}, {2, 3}, {0, 2}, {1, 3}, {2, 4}}));
+        System.out.println("Output:\t" + roads.maximumImportance(5, new int[][]{{0, 3}, {2, 4}, {1, 3}}));
+    }
+
+    public long maximumImportance(int n, int[][] roads) {
+        long result = 0;
+        int[] edges = new int[n];
+
+        for (int[] road : roads) {
+            edges[road[0]]++;
+            edges[road[1]]++;
+        }
+
+        Arrays.sort(edges);
+
+        for (int i = n - 1; i >= 0; i--) {
+            result = result + (long) edges[i] * (long) (i + 1);
+        }
+        return result;
+    }
+}
