523d31e579e7088f190d691435eff63d719ff98e
Henry Darana <h.darana@gmail.com>
2024-07-16 15:36:56 +0530

    MinimumAbsoluteDifferenceInBST, MinimumDistanceBetweenBSTNodes, PowerOfFour and RottingOranges

---
+++ src/main/java/algorithms/easy/MinimumDistanceBetweenBSTNodes.java
@@ -0,0 +1,31 @@
+//https://leetcode.com/problems/minimum-distance-between-bst-nodes/description/
+package algorithms.easy;
+
+import context.TreeNode;
+
+public class MinimumDistanceBetweenBSTNodes {
+    public static void main(String[] args) {
+        MinimumDistanceBetweenBSTNodes minDist = new MinimumDistanceBetweenBSTNodes();
+        System.out.println("Output:\t" + minDist.minDiffInBST(
+                new TreeNode(4, new TreeNode(2, new TreeNode(1), new TreeNode(3)), new TreeNode(6))));
+        System.out.println("Output:\t" + minDist.minDiffInBST(
+                new TreeNode(1, new TreeNode(0), new TreeNode(48, new TreeNode(12), new TreeNode(49)))));
+    }
+
+    int output = Integer.MAX_VALUE;
+    TreeNode previous = null;
+
+    public int minDiffInBST(TreeNode root) {
+        if (root == null) return output;
+
+        minDiffInBST(root.left);
+
+        if (previous != null) output = Math.min(output, root.val - previous.val);
+
+        if (root != null) previous = root;
+
+        minDiffInBST(root.right);
+        return output;
+    }
+
+}
