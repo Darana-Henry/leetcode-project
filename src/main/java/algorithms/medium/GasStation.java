1e8f2caa6de2cf7c909bd63be4042d321ae60601
Henry Darana <h.darana@gmail.com>
2024-07-24 21:25:19 +0530

    DailyTemperatures, FizzBuzz, GasStation and NextGreaterElementI

---
+++ src/main/java/algorithms/medium/GasStation.java
@@ -0,0 +1,24 @@
+//https://leetcode.com/problems/gas-station/description/
+package algorithms.medium;
+
+public class GasStation {
+    public static void main(String[] args) {
+        GasStation gas = new GasStation();
+        System.out.println("Output:\t" + gas.canCompleteCircuit(new int[]{1, 2, 3, 4, 5}, new int[]{3, 4, 5, 1, 2}));
+        System.out.println("Output:\t" + gas.canCompleteCircuit(new int[]{2, 3, 4}, new int[]{3, 4, 3}));
+    }
+
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int sum = 0, totalSum = 0, position = 0;
+        for (int i = 0; i < gas.length; i++) {
+            sum += gas[i] - cost[i];
+            if (sum < 0) {
+                totalSum += sum;
+                sum = 0;
+                position = i + 1;
+            }
+        }
+        totalSum += sum;
+        return totalSum >= 0 ? position : -1;
+    }
+}
