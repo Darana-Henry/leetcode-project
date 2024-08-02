53db466a2c66aea5b8f5fbf0b7c23b2d22be356a
Henry Darana <h.darana@gmail.com>
2024-08-02 20:09:30 +0530

    HammingDistance , NumberOfSeniorCitizens amd  ValidSudoku #1 || IslandPerimeter, SpiralMatrix

--- src/main/java/algorithms/easy/HammingDistance.java
+++ src/main/java/algorithms/easy/HammingDistance.java
@@ -2,6 +2,12 @@
 package algorithms.easy;
 
 public class HammingDistance {
+    public static void main(String[] args) {
+        HammingDistance hamming = new HammingDistance();
+        System.out.println("Output:\t" + hamming.hammingDistance(1, 4));
+        System.out.println("Output:\t" + hamming.hammingDistance(1, 3));
+    }
+
     public int hammingDistance(int x, int y) {
         int xor = x ^ y;
         int hamming = 0;
