fd9ac59b56214f311d0a38468e72086d5fa06b99
Henry Darana <h.darana@gmail.com>
2024-08-04 19:04:36 +0530

    ComplementOfBase10Integer, LicenseKeyFormatting, NumberComplement, RotateImage and ThreeSumClosest

---
+++ src/main/java/algorithms/easy/NumberComplement.java
@@ -0,0 +1,21 @@
+//https://leetcode.com/problems/number-complement/
+package algorithms.easy;
+
+public class NumberComplement {
+    public static void main(String[] args) {
+        NumberComplement complement = new NumberComplement();
+        System.out.println("Output:\t" + complement.findComplement(5));
+        System.out.println("Output:\t" + complement.findComplement(1));
+    }
+
+    public int findComplement(int num) {
+        int answer = 0;
+        int power = 1;
+        while (num > 0) {
+            answer += ((num % 2) ^ 1) * power;
+            power *= 2;
+            num /= 2;
+        }
+        return answer;
+    }
+}
