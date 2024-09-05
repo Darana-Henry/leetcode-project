c291e658194a41005a28cd39114497f71401e15b
Henry Darana <h.darana@gmail.com>
2024-09-05 20:06:35 +0530

    ImageSmoother, MaximalSquare, SumOfDigitsOfStringAfterConvert and UniqueBinarySearchTreesII

---
+++ src/main/java/algorithms/easy/SumOfDigitsOfStringAfterConvert.java
@@ -0,0 +1,28 @@
+//https://leetcode.com/problems/sum-of-digits-of-string-after-convert/
+package algorithms.easy;
+
+public class SumOfDigitsOfStringAfterConvert {
+    public static void main(String[] args) {
+        SumOfDigitsOfStringAfterConvert convert = new SumOfDigitsOfStringAfterConvert();
+        System.out.println("Output:\t" + convert.getLucky("iiii", 1));
+        System.out.println("Output:\t" + convert.getLucky("leetcode", 2));
+        System.out.println("Output:\t" + convert.getLucky("zbax", 2));
+    }
+
+    public int getLucky(String s, int k) {
+        StringBuilder sb = new StringBuilder();
+
+        for (char ch : s.toCharArray())
+            sb.append((ch - 'a') + 1);
+
+        String sum = sb.toString();
+        for (int i = 1; i <= k; i++) {
+            int transformedSum = 0;
+            for (char ch : sum.toCharArray())
+                transformedSum += ch - '0';
+
+            sum = String.valueOf(transformedSum);
+        }
+        return Integer.parseInt(sum);
+    }
+}
